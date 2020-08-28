4bad295162d712c2da1e310f61c9019752011ec4
Lindsay W <lwhite5314@gmail.com>
2020-08-27 20:51:35 -0400

    works

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -2,25 +2,37 @@ const db = require("../data/config")
 
 
 function find(){
-
+    return db('schemes')
 }
 
-function findBtId(id){
-
+function findById(id){
+    return db('schemes')
+    .where({id}).first()
 }
 
 function findSteps(id){
-
+    return db('steps as s')
+    .join("schemes", "schemes.id", "s.scheme_id")
+    .where("s.scheme_id", id)
+    .select("s.scheme_id", "s.step_number", "s.instructions")
 }
-
-function add(scheme){
-
+function add(scheme) {
+  return db("schemes")
+    .insert(scheme, "id")
 }
 
 function update(changes, id){
-
+    return db("schemes")
+    .where("id", id)
+    .update(changes)
 }
 
 function remove(id){
-    
+    return db('schemes')
+    .where("id", id)
+    .del()
+}
+
+module.exports= {
+    find, findById, findSteps, add, update, remove
 }
